ece0ab488055776424a4227949cd9fa1088402c9
Apostolos Bessas <mpessas@gmail.com>
2011-03-25 08:52:35 +0200

    Initial commit

---
+++ aur_search.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+import urllib
+
+
+URL = 'http://aur.archlinux.org/rpc.php'
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
